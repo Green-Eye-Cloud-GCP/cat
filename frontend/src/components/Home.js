84cb9eaf670d41aaef0b690e1bc3fd5f2b32e768
Andres Masanelli <masanelli.a@gmail.com>
2022-04-07 12:31:34 -0300

    show comprobantes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { Container, Table, Button, Row, Col, Pagination } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Table, Button, Row, Col, Pagination, Placeholder } from 'react-bootstrap';
+import { Trash, Pencil } from 'react-bootstrap-icons';
 
 const Home = () => {
 
-    const [comprobantes, setComprobantes] = useState([]);
+    const [comprobantes, setComprobantes] = useState();
 
     let active = 2;
     const items = [];
@@ -16,14 +18,50 @@ const Home = () => {
         );
     }
 
+    function TableContent(props) {
+        if (comprobantes) {
+            return comprobantes.map((comprobante, i) => {
+                return (
+                    <tr key={'row' + i}>
+                        <td>{comprobante.fecha.toLocaleDateString()}</td>
+                        <td>{comprobante.origenes.length}</td>
+                        <td>{comprobante.destino.name}</td>
+                        <td>{comprobante.cantidad}</td>
+                        <td>{comprobante.user}</td>
+                        <td>
+                            <Container className='center'>
+                                <Pencil className='mx-1' color={'green'} />
+                                <Trash className='mx-1' color={'red'} />
+                            </Container>
+                        </td>
+                    </tr>
+                )
+            })
+        }
+
+        return (
+            <tr>
+                <td><Placeholder animation='glow'><Placeholder className='w-100' /></Placeholder></td>
+                <td><Placeholder animation='glow'><Placeholder className='w-100' /></Placeholder></td>
+                <td><Placeholder animation='glow'><Placeholder className='w-100' /></Placeholder></td>
+                <td><Placeholder animation='glow'><Placeholder className='w-100' /></Placeholder></td>
+                <td><Placeholder animation='glow'><Placeholder className='w-100' /></Placeholder></td>
+                <td><Placeholder animation='glow'><Placeholder className='w-100' /></Placeholder></td>
+            </tr>
+        )
+    }
+
     useEffect(() => {
         Axios.get('/api/comprobantes', {
             params: {
-                token: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjIwYTNiYjczMzlhNGY3ZDY1M2FmNTkiLCJvcmciOiJhZGJsaWNrIiwicm9sZXMiOlsiY3Vwb3MuZGVhbGVyIiwiY2F0LmVkaXRvciJdLCJpYXQiOjE2NDkyOTgyNTksImV4cCI6MTY0OTM4NDY1OX0.DEdXG743c_cRKtFuhpajf3MW_Pe8lNqYTv9-e8hEhAjHif29tiMfAMD4dfMAZUfDpKlS373sa1U7zEV5mxa63A"
+                token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjIwYTNiYjczMzlhNGY3ZDY1M2FmNTkiLCJvcmciOiJhZGJsaWNrIiwicm9sZXMiOlsiY3Vwb3MuZGVhbGVyIiwiY2F0LmVkaXRvciJdLCJpYXQiOjE2NDkyOTgyNTksImV4cCI6MTY0OTM4NDY1OX0.DEdXG743c_cRKtFuhpajf3MW_Pe8lNqYTv9-e8hEhAjHif29tiMfAMD4dfMAZUfDpKlS373sa1U7zEV5mxa63A'
             }
         })
             .then((response) => {
-                setComprobantes(response.data);
+                setComprobantes(response.data.map(comprobante => {
+                    comprobante.fecha = new Date(comprobante.fecha);
+                    return comprobante;
+                }));
             })
             .catch((error) => {
                 console.log(error);
@@ -33,60 +71,36 @@ const Home = () => {
     return (
         <Container className='bg-light border pt-5'>
             <Row className='mb-3'>
-                <Col className='border'>
+                <Link to='/nuevo'>
                     <Button className='float-end' variant='primary'>Nuevo</Button>
-                </Col>
+                </Link>
             </Row>
             <Row>
                 <Col>
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>Fecha</th>
-                                <th>Origenes</th>
-                                <th>Destino</th>
-                                <th>Cantidad</th>
-                                <th>Usuario</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {
-                        comprobantes.map((comprobante) => {
-                            return (
+                    <div className="overflow-auto">
+                        <Table striped bordered hover >
+                            <thead>
                                 <tr>
-                                <td>{comprobante.fecha}</td>
-                                <td>{comprobante.origenes.length}</td>
-                                <td>{comprobante.destino.name}</td>
-                                <td>{comprobante.cantidad}</td>
-                                <td>{comprobante.user}</td>
-                            </tr>
-                            )
-                        })
-                        }
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td colSpan={2}>Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </Table>
+                                    <th>Fecha</th>
+                                    <th>Origenes</th>
+                                    <th>Destino</th>
+                                    <th>Cantidad</th>
+                                    <th>Usuario</th>
+                                    <th>Opciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <TableContent />
+                            </tbody>
+                        </Table>
+                    </div>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <Pagination className='float-end'>{items}</Pagination>
+                    {
+                        comprobantes && <Pagination className='float-end'>{items}</Pagination>
+                    }
                 </Col>
             </Row>
 
