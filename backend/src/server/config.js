e7cff45e3feee0cc1bbbfddc6946476a595e3839
Andres Masanelli <masanelli.a@gmail.com>
2022-04-10 22:27:00 -0300

    deploy

--- backend/src/server/config.js
+++ backend/src/server/config.js
@@ -1,4 +1,4 @@
-const MONGODB_URI = undefined; //'projects/214203301889/secrets/MONGODB_URI/versions/1';
+const MONGODB_URI = 'projects/214203301889/secrets/MONGODB_URI/versions/1';
 const JWT_PRIVATE = 'projects/214203301889/secrets/JWT_PRIVATE/versions/2';
 const JWT_PUBLIC = '-----BEGIN PUBLIC KEY-----\nMFswDQYJKoZIhvcNAQEBBQADSgAwRwJAZvt/mMOXuvBvz8FCHcgdM5CUKrix+u53\nhrGQZNpI+2gun8fE7Muk1kvErIbjOS6mDIWlmMaa5IckNVltExqu2wIDAQAB\n-----END PUBLIC KEY-----';
 const GREEN_EYE_URL = 'https://www.greeneye.cloud';
